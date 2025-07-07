672a1ab0ec94ed8e3fe703df578c72ec06a0f63e
AhmedelAraby0 <a.a.elaraby0@gmail.com>
2025-07-07 13:46:58 +0300

    Add files via upload

---
+++ translation-service-final-main/schemas.py
@@ -0,0 +1,22 @@
+
+"""
+I always insist on using schemas to have full control of the data flow from fe -> be.
+"""
+
+
+
+# schemas.py
+
+from pydantic import BaseModel
+
+class TranslationRequestSchema(BaseModel):
+    text: str
+    languages: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "text": "Hello, world!",
+                "languages": "english, german, russian"
+            }
+        }
